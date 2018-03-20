0415bf95aa61111d9bb15dd3e13c631c986092cf
Lukasz Antkowiak <lukasz.antkowiak@siili.com>
2018-03-20 15:33:47 +0100

    inserting contacts

--- src/main/java/com/siili/heroku/poc/controllers/ContactsController.java
+++ src/main/java/com/siili/heroku/poc/controllers/ContactsController.java
@@ -2,7 +2,9 @@ package com.siili.heroku.poc.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,10 +19,19 @@ public class ContactsController {
     @RequestMapping("/")
     public List<Map<String, Object>> getContacts() {
         System.out.println("ContactsController.getContacts");
-        return jdbcTemplate.queryForList("select * from salesforce.Contact");
+        return jdbcTemplate.queryForList("select firstname, lastname, private_email__c from salesforce.Contact");
     }
 
-    @RequestMapping("/2")
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public int addContact(@RequestBody String firstName, @RequestBody String lastName, @RequestBody String privateEmail) {
+        int update = jdbcTemplate.update(
+                "INSERT INTO salesforce.Contact (firstname, lastname, private_email__c) VALUES (?, ?, ?)",
+                firstName, lastName, privateEmail
+        );
+        return update;
+    }
+
+    @RequestMapping("/status")
     public String ok() {
         return "OK";
     }
